083be960f0f34cac4bcb4799c9b2033b40e3a37a
riostamires <tamires.rios@acad.pucrs.br>
2020-06-24 11:06:13 -0300

    Eu errei o outro commit

---
+++ exercicios2/src/Tabuada.java
@@ -0,0 +1,10 @@
+public class Tabuada {
+    public static void main(String[] args) {
+            System.out.println("Este programa irá exibir a tabuada do 9 abaixo:");
+        int i = 9;
+        int j = 0;
+        while (j<=10) {
+            System.out.println(i + " x " + j + " = " + i * j);
+            j = j+1; }
+    }
+}
